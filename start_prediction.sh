5ed23b08c687374e78852f457db45d86eddde7a9
Leonard Konle <leonardkonle@gmail.com>
2022-11-15 13:01:22 +0100

    Update start_prediction.sh

--- start_prediction.sh
+++ start_prediction.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-python3 predict_shaver.py -modelname "Sadness.pt" 
-python3 predict_shaver.py -modelname "Love.pt" 
-python3 predict_shaver.py -modelname "Freude.pt" 
-python3 predict_shaver.py -modelname "Fear.pt" 
-python3 predict_shaver.py -modelname "Anger.pt" 
-python3 predict_shaver.py -modelname "Agitation.pt"
+python3 predict_shaver.py -inputfile "example.tsv" -modelname "Sadness.pt" 
+python3 predict_shaver.py -inputfile "example.tsv" -modelname "Love.pt" 
+python3 predict_shaver.py -inputfile "example.tsv" -modelname "Freude.pt" 
+python3 predict_shaver.py -inputfile "example.tsv" -modelname "Fear.pt" 
+python3 predict_shaver.py -inputfile "example.tsv" -modelname "Anger.pt" 
+python3 predict_shaver.py -inputfile "example.tsv" -modelname "Agitation.pt"
 
